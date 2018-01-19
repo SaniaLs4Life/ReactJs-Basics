6495f7b29de6a48447a34da0edf88510ee40a5b0
Hakan <sanials4life2@gmail.com>
2018-01-19 15:44:53 +0100

    :rocket:

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="container">
+          <button className="btn btn-primary">Click Me!</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
